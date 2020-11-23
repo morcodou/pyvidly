5abe104a0aea56489f8e26e57d833dadd56edb01
Apple <morcodou@hotmail.com>
2020-11-23 00:02:44 -0500

    sending movies as respone

--- movies/views.py
+++ movies/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from .models import Movie
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("Hello Word")
+    moives = Movie.objects.all()
+    output = ", ".join([m.title for m in moives])
+    return HttpResponse(output)
